d715205ba6907828c43230a035ec710c6e3b4ef7
Lavender <gercermagden@gmail.com>
2025-05-10 19:03:48 -0500

    refactor: modificar inyección de dependencia en UsuarioService
    Se eliminó el modificador private y se cambió la llamada a persistencia,
    inyectando directamente UsuarioJpaController en lugar del repository anterior.

--- src/main/java/com/chattide/web/Service/UsuarioService.java
+++ src/main/java/com/chattide/web/Service/UsuarioService.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
 public class UsuarioService implements IUsuarioService {
 
     @Inject
-    private UsuarioJpaController usuarioJpaController;
+    UsuarioJpaController usuarioJpaController;
 
     public UsuarioService() {
         this.usuarioJpaController = new UsuarioJpaController();
