1c6474d32c93a2778003e7aa66cd70dba5031ec6
Luis Vallejos <vallejosevangelistaluis@gmail.com>
2025-05-14 17:45:42 -0500

    feat(service): extender IComentarioService con métodos de búsqueda
    - Se añaden los métodos findByPublicacion(Long) y findByUsuario(Long) a la interfaz IComentarioService.
    - Facilita la implementación de la lógica de búsqueda de comentarios por publicación y usuario.

--- src/main/java/com/chattide/web/Service/IComentarioService.java
+++ src/main/java/com/chattide/web/Service/IComentarioService.java
@@ -1,12 +1,15 @@
 package com.chattide.web.Service;
 
+import com.chattide.web.DTO.ComentarioDTO;
 import com.chattide.web.Modelo.Comentario;
 import com.chattide.web.Service.generic.ICrudService;
+import java.util.List;
 
 /**
  *
  * @author Juan - Luis
  */
 public interface IComentarioService extends ICrudService<Comentario, Long> {
-
+    List<ComentarioDTO> findByPublicacion(Long publicacionId);
+    List<Comentario> findByUsuario(Long usuarioId);
 }
