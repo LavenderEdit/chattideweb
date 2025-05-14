8d093586fa4e3bcbd45abaad3e1827e814c90626
Lavender <gercermagden@gmail.com>
2025-05-14 13:20:36 -0500

    feat(registry): actualizar registro de componentes y versionado
    Cambios realizados:
    
    1. Gestión de versiones:
    - Añadido control de versión (?v=1) a usuario-controller.js
    - Mantenido versión en script-functions.js
    
    2. Ampliación de rutas:
    - Añadido soporte para 'SvMiCuenta' junto a 'miCuenta.jsp'
    - Unificado manejo de rutas relacionadas (grupos y cuenta)
    
    3. Nuevas funcionalidades:
    - Inicialización de UsuarioController en página de cuenta
    - Activación de checkArchiveSizeAndType para subida de archivos
    - Mantenimiento de funcionalidad existente para grupos
    
    4. Mejoras técnicas:
    - Eliminado comentario redundante
    - Organización más clara de casos
    - Preparación para futuras extensiones
    
    Impacto:
    - Mejor control de caché con versionado
    - Soporte para URLs de servlets y JSPs
    - Funcionalidades adicionales en gestión de cuenta
    - Código más mantenible

--- src/main/webapp/js/script-registry.js
+++ src/main/webapp/js/script-registry.js
@@ -1,5 +1,5 @@
 import { togglePasswordVisibility, checkArchiveSizeAndType } from './lib/utils/script-functions.js?v=1';
-import UsuarioController from './controllers/usuario-controller.js';
+import UsuarioController from './controllers/usuario-controller.js?v=1';
 import { initExitGroupButtons } from './controllers/grupo-controller.js';
 
 export function runComponentRegistry() {
@@ -19,14 +19,15 @@ export function runComponentRegistry() {
             break;
         case 'miGrupo.jsp':
         case 'SvMiGrupo':
-            // Agrega funciones para miGrupo.jsp si es necesario
-            break;
         case 'misGrupos.jsp':
         case 'SvMisGrupos':
             initExitGroupButtons();
             // Agrega funciones para misGrupos.jsp si es necesario
             break;
         case 'miCuenta.jsp':
+        case 'SvMiCuenta':
+            checkArchiveSizeAndType();
+            UsuarioController.init();
             // Agrega funciones para miCuenta.jsp si es necesario
             break;
         case 'publicacion.jsp':
