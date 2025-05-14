6870c831a350d3ebdc82a0293cc61006c4991029
Luis Vallejos <vallejosevangelistaluis@gmail.com>
2025-05-14 16:15:25 -0500

    feat(interface): declarar método findDTOByGrupo en IPublicacionService
    Se agrega la firma del método findDTOByGrupo para exponer la conversión 
    de entidades Publicacion a DTO en base al ID del grupo.

--- src/main/java/com/chattide/web/Service/IPublicacionService.java
+++ src/main/java/com/chattide/web/Service/IPublicacionService.java
@@ -1,12 +1,14 @@
 package com.chattide.web.Service;
 
+import com.chattide.web.DTO.PublicacionDTO;
 import com.chattide.web.Modelo.Publicacion;
 import com.chattide.web.Service.generic.ICrudService;
+import java.util.List;
 
 /**
  *
  * @author Juan - Luis
  */
 public interface IPublicacionService extends ICrudService<Publicacion, Long> {
-    
+    List<PublicacionDTO> findDTOByGrupo(Long grupoId);
 }
