3ad2c763262ea3f89c3742896b84ea9d50f0cff9
Lavender <gercermagden@gmail.com>
2025-05-15 23:11:35 -0500

    refactor(script-registry): optimizar registro de componentes y añadir soporte para likes
    - Extraer lógica de obtención de nombre de página a función dedicada (getPageName)
    - Añadir inicialización de controlador de likes (initLikeButtons) en:
      * Páginas de grupo (miGrupo.jsp)
      * Páginas de publicación (publicacion.jsp)
    - Mejorar obtención de IDs usando URLSearchParams
    - Actualizar versiones de scripts importados:
      * publicacion-controller.js a v1
      * comentario-controller.js a v2
    - Limpiar estructura switch-case:
      * Eliminar comentarios redundantes
      * Agregar breaks faltantes
      * Simplificar caso default
    - Mejorar formato de imports (alineación vertical)

--- src/main/webapp/js/script-registry.js
+++ src/main/webapp/js/script-registry.js
@@ -1,53 +1,58 @@
 import { togglePasswordVisibility, checkArchiveSizeAndType } from './lib/utils/script-functions.js?v=1';
 import UsuarioController from './controllers/usuario-controller.js?v=1';
-import { initExitGroupButtons } from './controllers/grupo-controller.js';
-import PublicacionController from './controllers/publicacion-controller.js';
-import ComentarioController from './controllers/comentario-controller.js?v=1';
+import { initExitGroupButtons }     from './controllers/grupo-controller.js';
+import PublicacionController        from './controllers/publicacion-controller.js?v=1';
+import ComentarioController         from './controllers/comentario-controller.js?v=2';
+import { initLikeButtons }          from './controllers/like-controller.js';
 
+function getPageName() {
+    const withoutQuery = window.location.href.split('?')[0];
+    return withoutQuery.substring(withoutQuery.lastIndexOf('/') + 1);
+}
 
 export function runComponentRegistry() {
-    const path = window.location.pathname;
-    const pageName = path.substring(path.lastIndexOf('/') + 1);
+    const pageName = getPageName();
 
     switch (pageName) {
         case 'registro.jsp':
-            // Ejecuta funciones específicas para la página de registro
             togglePasswordVisibility();
             checkArchiveSizeAndType();
             break;
+
         case 'login.jsp':
-            // Ejecuta funciones específicas para la página de login
             UsuarioController.init();
             togglePasswordVisibility();
             break;
+
         case 'miGrupo.jsp':
         case 'SvMiGrupo':
             initExitGroupButtons();
-            const gid = document.body.dataset.grupoId;
-            PublicacionController.init(gid);
+            initLikeButtons();
+            const gid = new URLSearchParams(window.location.search).get('id');
+            if (gid)
+                PublicacionController.init(gid);
+            break;
+
         case 'misGrupos.jsp':
         case 'SvMisGrupos':
             initExitGroupButtons();
-            // Agrega funciones para misGrupos.jsp si es necesario
             break;
+
         case 'miCuenta.jsp':
         case 'SvMiCuenta':
             checkArchiveSizeAndType();
             UsuarioController.init();
-            // Agrega funciones para miCuenta.jsp si es necesario
             break;
+
         case 'publicacion.jsp':
         case 'SvPublicacion':
-            const pid = document.body.dataset.grupoId;
-            ComentarioController.init(pid);
-            // Agrega funciones para publicacion.jsp si es necesario
-            break;
-        case 'crearGrupo.jsp':
-            // Agrega funciones para crearGrupo.jsp si es necesario
+            const pid = new URLSearchParams(window.location.search).get('id');
+            if (pid) {
+                ComentarioController.init(pid);
+                initLikeButtons();
+            }
             break;
         default:
-            // Opcional: alguna función por defecto
-            break;
+        // nada
     }
 }
-
